f0a4764bc067275d0712f63088a805e6f24daef9
임동혁 <idh1007@naver.com>
2022-10-28 15:47:59 +0900

    [ADD] 문장에서 단어 추출 기능 구현

---
+++ extractWord.py
@@ -0,0 +1,25 @@
+from konlpy.tag import Okt
+
+
+def eWord(sentence):
+    print(sentence)
+    twt = Okt()
+    tagging = twt.pos(sentence)
+
+    v = []
+    sy = []
+    for i, j in tagging:
+        if j == 'Noun' or j == 'Verb' or j == 'Adjective':
+            if j == 'Verb':
+                v.append(i)
+            else:
+                sy.append(i)
+
+    sy.append(v[-1])
+
+    if '사람' in sy:
+        sy.remove('사람')
+        return sy
+    else:
+        return sy
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+import extractWord as EW
 
 Recognizer = sr.Recognizer()  # 인스턴스 생성
 mic = sr.Microphone()
@@ -7,14 +8,15 @@ while True:
     inputText = input("음성 인식을 위해 'start' 를 입력하세요 : ");
 
     if inputText == 'start':
-        with mic as source:  # 안녕~이라고 말하면
-            audio = Recognizer.listen(source)
-        try:
-            data = Recognizer.recognize_google(audio, language="ko")
-        except:
-            print("이해하지 못했음")
+        # with mic as source:  # 안녕~이라고 말하면
+        #     audio = Recognizer.listen(source)
+        # try:
+        #     data = Recognizer.recognize_google(audio, language="ko")
+        # except:
+        #     print("이해하지 못했음")
 
-        print(data)  # 안녕 출력
+        result = EW.eWord("빨간 모자 쓴 사람 찾아줘")
+        print(result)  # 안녕 출력
 
     elif inputText == 'end':
         exit()
