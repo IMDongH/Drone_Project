038c2b833128039f727c6d9ae9f7f1a930d886bc
임동혁 <idh1007@naver.com>
2022-11-04 19:54:41 +0900

    extract word 로직 추가

--- extractWord.py
+++ extractWord.py
@@ -1,24 +1,21 @@
-from konlpy.tag import Okt
-
+objectList = ['bottle', 'cap', 'person']
+actionList = ['follow','turn','hungry']
+colorList = ['red','orange','yellow','green','blue','purple','black','white']
+stopWord = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 def eWord(sentence):
-    twt = Okt()
-    tagging = twt.pos(sentence)
 
-    v = []
-    sy = []
-    for i, j in tagging:
-        if j == 'Noun' or j == 'Verb' or j == 'Adjective':
-            if j == 'Verb':
-                v.append(i)
-            else:
-                sy.append(i)
+    words = sentence.split() #토큰화 간소화
 
-    if len(v) != 0:
-        sy.append(v[-1])
+    result = {}
+    for w in words :
+        if w not in stopWord: # 불용어 처리
+            if w in objectList:
+                result["object"] = words[i]
+            elif w in actionList:
+                result["action"] = words[i]
+            elif w in colorList:
+                result["color"] = words[i]
 
-    if '사람' in sy:
-        sy.remove('사람')
-        return sy
-    else:
-        return sy
+    return result
+
