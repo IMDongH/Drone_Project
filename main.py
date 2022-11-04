6cab9a9774e37c6de711e0fb7009f634f182e053
Owen-Choi <oldstyle482@gmail.com>
2022-11-04 23:21:58 +0900

    [ADD] 롤백예정 - checkLocation 변경

--- main.py
+++ main.py
@@ -19,62 +19,60 @@ height = 630
 frameWidth = width
 frameHeight = height
 # objectList = ['cup', 'person', 'cell phone']
-objectList = ['bottle']
+objectList = ['person']
 ###########################################
 
-def checkLocation(result):
-    for i, data in enumerate(result):
-        if data[0] in objectList:
-            if (data[3] - data[1]) > width/3 or (data[4] - data[2]) > height/3:
-                # print('go_back()', (data[3] - data[1]) * (data[4] - data[2]))
-                # print('go_back()', (data[3] - data[1]) * (data[4] - data[2]))
-                myDrone.move_back(20)
-            elif (data[3] - data[1]) < (width/3)*1/3 or (data[4] - data[2]) < (height/3)*1/3:
-                # print('go_front()', (data[3] - data[1]) * (data[4] - data[2]))
-                myDrone.move_forward(20)
-            else:
-                if data[1] < 210 and data[2] < 210:
-                    print("1")
-                    myDrone.rotate_clockwise()
-                if data[1] > 210 and data[1] < 420 and data[2] < 210:
-                    print("2")
-                if data[1] > 420 and data[1] < 630 and data[2] < 210:
-                    print("3")
+def checkLocation(offset_x, offset_y, offset_z):
+    """
+    Adjusts the position of the tello drone based on the offset values given from the frame
 
-                if data[1] < 210 and data[2] > 210 and data[2] < 420:
-                    print("4")
-                if data[1] > 210 and data[1] < 420 and data[2] > 210 and data[2] < 420:
-                    print("5")
-                if data[1] > 420 and data[1] < 630 and data[2] > 210 and data[2] < 420:
-                    print("6")
+    :param offset_x: Offset between center and face x coordinates
+    :param offset_y: Offset between center and face y coordinates
+    :param offset_z: Area of the face detection rectangle on the frame
+    """
 
-                if data[1] < 210 and data[2] > 420 and data[2] < 630:
-                    print("7")
-                if data[1] > 210 and data[1] < 420 and data[2] > 420 and data[2] < 630:
-                    print("8")
-                if data[1] > 420 and data[1] < 630 and data[2] > 420 and data[2] < 630:
-                    print("9")
+    print(offset_x, offset_y, offset_z)
+
+    if not -90 <= offset_x <= 90 and offset_x is not 0:
+        if offset_x < 0:
+            myDrone.rotate_counter_clockwise(30)
+        elif offset_x > 0:
+            myDrone.rotate_clockwise(30)
+
+    if not -70 <= offset_y <= 70 and offset_y is not -30:
+        if offset_y < 0:
+            myDrone.move_up(20)
+        elif offset_y > 0:
+            myDrone.move_down(20)
+
+    if not 15000 <= offset_z <= 30000 and offset_z is not 0:
+        if offset_z < 15000:
+            myDrone.move_forward(40)
+        elif offset_z > 30000:
+            myDrone.move_back(40)
+
+
 
 ###########################################
 model = Model('/Users/oldst/PycharmProjects/Drone_Project/en')
 recognizer = KaldiRecognizer(model,16000)
-#
+
 print("please speak to dongchul : ")
 cap = pyaudio.PyAudio()
 stream = cap.open(format=pyaudio.paInt16, channels=1,rate=16000,input=True, frames_per_buffer=8192)
 stream.start_stream()
 
-while True:
-    data = stream.read(4096)
-    # if len(data) == 0:
-    #     break
-
-    if recognizer.AcceptWaveform(data):
-        # print(recognizer.Result())
-        jsonObject = json.loads(recognizer.Result())
-        # print(jsonObject.get('text').split())
-        print(EW.eWord(jsonObject.get('text').split()))
-        break
+# while True:
+#     data = stream.read(4096)
+#     # if len(data) == 0:
+#     #     break
+#
+#     if recognizer.AcceptWaveform(data):
+#         # print(recognizer.Result())
+#         jsonObject = json.loads(recognizer.Result())
+#         # print(jsonObject.get('text').split())
+#         print(EW.eWord(jsonObject.get('text').split()))
+#         break
 
 myDrone = Tello()
 myDrone.connect()
@@ -87,7 +85,7 @@ myDrone.streamon()
 myDrone.takeoff()
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model = torch.load("./model_data.pt", map_location=device)
+model = torch.load("./model_data_yolov5n.pt", map_location=device)
 
 while True:
     img = myDrone.get_frame_read().frame
@@ -95,14 +93,20 @@ while True:
     results = model(img)
     results2 = results.pandas().xyxy[0][['name', 'xmin', 'ymin', 'xmax', 'ymax']]
 
-    checkLocation(results2.values)
+    center_x = int(width / 2)
+    center_y = int(height / 2)
 
     for num, i in enumerate(results2.values):
-        if i[0] == 'bottle':
-            cv.putText(img, i[0], ((int(i[1]), int(i[2]))), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
-            cv.rectangle(img, (int(i[1]), int(i[2])), (int(i[3]), int(i[4])), (0, 0, 255), 3)
-
-
+            if i[0] == 'bottle':
+                cv.putText(img, i[0], ((int(i[1]), int(i[2]))), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+                cv.rectangle(img, (int(i[1]), int(i[2])), (int(i[3]), int(i[4])), (0, 0, 255), 3)
+                object_center_x = (i[3] + i[1]) / 2
+                object_center_y = (i[4] + i[2]) / 2
+                offset_x = object_center_x - center_x
+                offset_y = object_center_y - center_y - 30
+                offset_z = (i[3] - i[1]) * (i[4] - i[2])
+                checkLocation(offset_x, offset_y, offset_z)
+                break
     cv.imshow('temp', img)
     cv.waitKey(1)
 
